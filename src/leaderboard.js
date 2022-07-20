245678d60442e84c5454adca6e103ee7501526b0
Anas Abubakar <anasabubakar2015@gmail.com>
2022-07-20 20:27:50 +0100

    Fixing linter errors

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -3,13 +3,13 @@ const inputName = document.querySelector('#name');
 const inputScore = document.querySelector('#score');
 const submitBtn = document.querySelector('.btn');
 
-const addScore= () => {
+const addScore = () => {
   if (!inputName.value || !inputScore.value) return;
   const score = document.createElement('li');
   score.classList.add('list-item');
   score.innerText = `${inputName.value} : ${inputScore.value}`;
   scoreList.appendChild(score);
-}
+};
 
 export {
   addScore, scoreList, inputName, inputScore, submitBtn,
